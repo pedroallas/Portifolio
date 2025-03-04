6c5a0d368223357d29576e76ef7770f05ca6e007
Pedro Allas dos Santos Borges <pedro.borges@faculdadegran.edu.br>
2025-03-03 23:21:20 -0300

    feat add vercel new

--- src/Pages/Home/sections/ProjectsSection/ProjectsSection.tsx
+++ src/Pages/Home/sections/ProjectsSection/ProjectsSection.tsx
@@ -6,14 +6,13 @@ const ProjectsSection: React.FC = () => {
 
     const StyledExperience = styled("div")(({ theme }) => ({
         backgroundColor: theme.palette.primary.main,
-
     }));
 
     const projects = [
         {
             title: "Duallas Restaurant",
             subtitle: "Jul 2023 - Dez 2023",
-            srcImg: "/public/assets/images/project-trello.png",
+            srcImg: "/assets/images/project-trello.png",
             description: "Projeto desenvolvido durante a FSW cosiste em um sistema de gerenciamento de pedidos de um restaurante fictício chamado Duallas. O sistema permite que o usuário faça login, visualize o cardápio, adicione itens ao carrinho, remova itens do carrinho, visualize o carrinho, faça o pedido e visualize o pedido. O sistema também permite que o usuário faça logout e visualize o histórico de pedidos.", 
             technologies: "Technologies: JavaScript, HTML, CSS, Canvas Graphics",
             websiteURL: "https://pad-port-donalds-vkts.vercel.app/duallas-donalds",
@@ -22,7 +21,7 @@ const ProjectsSection: React.FC = () => {
         {
             title: "Project Exemple",
             subtitle: "Jul 2023 - Dez 2023",
-            srcImg: "/public/assets/images/project-financas.png",
+            srcImg: "/assets/images/project-financas.png",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porta semper velit vel rutrum. Aliquam vulputate, nisi eget tristique mattis, nisi sem faucibus eros, a auctor felis sem ut mauris. Phasellus a ultrices elit. Curabitur ut diam eu orci auctor pretium.",
             technologies: "Technologies: JavaScript, HTML, CSS, Canvas Graphics",
             websiteURL: "https://trello.com/",
@@ -31,7 +30,7 @@ const ProjectsSection: React.FC = () => {
         {
             title: "Project Craze Maze",
             subtitle: "Jul 2019 - May 2019",
-            srcImg: "/public/assets/images/project1-craze-maze.gif",
+            srcImg: "/assets/images/project1-craze-maze.gif",
             description: "Game to escape the maze, but not only that. An algorithm has been created that randomly generates a new maze each time the game is started. In this game, the user can use the keyboard keys to move until they find the flag and win the game",
             technologies: "Technologies: JavaScript, HTML, CSS, Canvas Graphics",
             websiteURL: "https://adrianasaty.github.io/ironhack-project1-craze-maze/index.html",
@@ -40,7 +39,7 @@ const ProjectsSection: React.FC = () => {
         {
             title: "Project Blotting",
             subtitle: "Jul 2019 - May 2019",
-            srcImg: "/public/assets/images/project2-blotting.png",
+            srcImg: "/assets/images/project2-blotting.png",
             description: "Game to escape the maze, but not only that. An algorithm has been created that randomly generates a new maze each time the game is started. In this game, the user can use the keyboard keys to move until they find the flag and win the game",
             technologies: "Technologies: JavaScript, HTML, CSS, Canvas Graphics",
             websiteURL: "https://adrianasaty.github.io/ironhack-project1-craze-maze/index.html",
